f5ab3614f0b82a19248ffa375454b580eb9931fc
Sanjay-George <sanjaygeorge16@gmail.com>
2019-05-31 09:32:17 +0530

    Set default timestamp of 1 week - Clean up code

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,16 +1,4 @@
-//const releaseData = require("../database/releaseData");
 const urlRepo = require("../database/urlRepository");
-
-//const apps = {
-//    bikewale : "bw",
-//    carwale : "cw"
-//}
-//const pages = {
-//    make: "makes"
-//    , model: "models"
-//    , pic: "priceincities"
-//, }
-
 
 module.exports = {
     homepage(req, res) {
@@ -21,14 +9,12 @@ module.exports = {
     },
     urlController(req, res){
         let data = {};
-        // TOOD : remove unnecessary params
-//        data.maskingName = req.params.maskingName;
         data.metric = "performance";
-        data.startDate = parseInt(req.query.start);
-        data.endDate = parseInt(req.query.end);
+        if(req.query.start == undefined || req.query.end == undefined)
+            oneWeekAgo = calculateDefaultTimestamp()
+        data.startDate = req.query.start != undefined ? parseInt(req.query.start) : oneWeekAgo;
+        data.endDate = req.query.end != undefined ? parseInt(req.query.end) : Date.now();
         data.urlId = req.params.urlid;
-//        data.app = apps.bikewale;
-//        data.page = pages.model;
         data.metrics = [];
         data.graphPoints = [];
         
@@ -49,56 +35,12 @@ module.exports = {
             }); 
         
     }
-//    bikewaleModels(req, res) {
-//            var data = {};
-//            data.metric = req.query.metric;
-//            data.startDate = req.query.startDate;
-//            data.endDate = req.query.endDate;
-//            data.app = apps.bikewale;
-//            data.page = pages.model;
-//            res.render('bikewale.ejs', data);
-//        }
-//        , bikewaleModelByName(req, res) {
-//            var data = {};
-//            data.maskingName = req.params.maskingName;
-//            data.metric = req.query.metric;
-//            data.startDate = req.query.startDate;
-//            data.endDate = req.query.endDate;
-//            data.app = apps.bikewale;
-//            data.page = pages.model;
-//            data.metrics = {};
-//            data.graphPoints = [];
-//            
-//            releaseData.fetchReleaseData(data.startDate, data.endDate).then(result => {
-//                console.log("FROM DB  : " + JSON.stringify(result));
-//                
-//                specificPageData.fetchDataByKey(data.app, data.page, data.maskingName, result.startTimestamp, result.endTimestamp, data.metric).then(result => {
-//                    // result is in array of objects format
-//                    // TODD : CALC WEIGHTED AVERAGE FOR EACH METRIC AND SET IT TO METRICS (VIEW IN TABLE)
-//                    data.metrics = result[0];                    
-//                    
-//                    for(var i = 0; i < result.length; i++){
-//                        if(result[i] != null){
-//                            var point = [];
-//                            point.push(new Date(parseInt(result[i].timestamp)).getTime());
-//                            point.push(result[i].performance.score);
-//
-//                            data.graphPoints.push(point);
-//                        }
-//                        
-//                    }
-//                    data.graphPoints = JSON.stringify(data.graphPoints);
-//                    
-//                    
-//                    console.log(JSON.stringify(data));
-//                    res.render('bikewale.ejs', data);
-//
-//                });
-//                
-//                
-//            })
-//        }
-//        , carwale(req, res) {
-//            res.render('carwale.ejs', {});
-//        }
+
+}
+
+function calculateDefaultTimestamp()
+{
+    var oneWeek = new Date();
+    oneWeek.setDate(oneWeek.getDate() - 30);
+    return oneWeek.getTime();
 }
